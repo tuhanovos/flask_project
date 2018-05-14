a334a9911a33c24448f5285463df4e184e0ae169
tuhanovos <saitpage2011@yandex.ru>
2018-05-14 21:33:36 +0300

    Ubuntu 18.04 install

--- README.md
+++
@@ -1,2 +0,0 @@
-# flask_project
-My first project on flask
--- bin/activate.fish
+++ bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/vladimir/workspace/flask_project"
+set -gx VIRTUAL_ENV "/home/eq/work/flask_project"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
